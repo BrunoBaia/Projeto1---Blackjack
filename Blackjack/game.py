8acb0e45beda1f467edce3da377acab7817a8464
BrunoBaia <bruno1397@gmail.com>
2023-02-16 20:37:13 -0300

    Completando codigo e mergendo

--- Blackjack/game.py
+++ Blackjack/game.py
@@ -1,9 +1,32 @@
 from models import *
 from time import sleep
+from random import randint
 
 
-def game(coin):
-    player = Blackjack()
+def opponent_turn(opponent: Blackjack) -> Blackjack:
+    print("\nVez do oponente!")
+    for value in range(2):
+        opponent.distribute_card()
+    opponent.initial_point(manual=False)
+    while True:
+        if opponent.point < 15:
+            opponent.distribute_card()
+            opponent.calculate_point(manual=False)
+            print("O oponente comprou uma carta...")
+            sleep(2)
+            if opponent.point > 21:
+                print(f"O oponente estorou com {opponent.point} pontos")
+                opponent.point = 0
+                sleep(2)
+                return opponent
+        else:
+            print(f"O oponente finalizou com {len(opponent.hand)} cartas na mao")
+            sleep(2)
+            return opponent
+
+
+def player_turn(player: Blackjack) -> Blackjack:
+    print("\nSua vez!")
     for value in range(2):
         player.distribute_card()
     player.show_hand()
@@ -11,30 +34,47 @@ def game(coin):
     print(f"Total de pontos: {player.point}")
     sleep(2)
     while True:
-        pick = input("\nDigite 'S' para pegar uma carta 'N' para manter mao: ")
+        pick = input("Digite 'S' para pegar uma carta 'N' para manter mao: ")
         if pick.upper() == "S":
             player.distribute_card()
             player.show_hand()
             player.calculate_point()
             if player.point > 21:
-                print(f"Voce perdeu, a pontuacao estorou em {player.point}\n")
-                coin -= 1
+                print(f"A pontuacao estorou em {player.point}")
+                player.point = 0
                 sleep(2)
-                break
+                return player
             print(f"Total de pontos: {player.point}")
             sleep(2)
         else:
-            break
-    continua = input("Digite 'S' para jogar novamente 'N' para parar: ")
-    if continua.upper() == "S":
-        return game(coin)
+            print(f"Voce finalizou com {len(player.hand)} cartas na mao")
+            sleep(2)
+            return player
+
+
+def game(coin, counter):
+    counter += 1
+    player = Blackjack()
+    opponent = Blackjack()
+    turn = randint(0, 1)
+    if turn:
+        player = player_turn(player)
+        opponent = opponent_turn(opponent)
     else:
-        print(f"Jogo finalizado, voce terminou com {coin} moedas")
+        opponent = opponent_turn(opponent)
+        player = player_turn(player)
+    coin = player.compare_results(opponent, coin)
+    continua = input("Digite 'S' para jogar novamente 'N' para parar: ")
+    if continua.upper() == "S" and coin > 0:
+        return game(coin, counter)
+    else:
+        print(f"\nJogo finalizado; \nNumero de partida(s): {counter}; \nMoedas iniciais: 5; \nMoedas finais: {coin}.")
 
 
 def main():
-    coin = 10
-    game(coin)
+    coin = 5
+    counter = 0
+    game(coin, counter)
 
 
 if __name__ == "__main__":
--- Blackjack/models.py
+++ Blackjack/models.py
@@ -24,36 +24,74 @@ class Blackjack(Deck):
 
     def __init__(self) -> None:
         super().__init__()
-        self.hand: list = []
-        self.point: int = 0
+        self.__hand: list = []
+        self.__point: int = 0
+
+    @property
+    def hand(self) -> list:
+        return self.__hand
+
+    @property
+    def point(self) -> int:
+        return self.__point
+
+    @hand.setter
+    def hand(self, new_value: List[tuple]) -> None:
+        self.__hand = new_value
+
+    @point.setter
+    def point(self, new_value: int) -> None:
+        self.__point = new_value
 
     def distribute_card(self) -> None:
         self.hand.append(self.deck.pop(0))
 
     def show_hand(self) -> None:
-        print("Suas cartas sao:")
+        print("Cartas:")
         for card in self.hand:
             print(*card)
 
-    def initial_point(self) -> None:
+    def initial_point(self, manual: bool = True) -> None:
         for card in self.hand:
-            self.point += self._convert_to_digit(card[0])
+            self.point += self._convert_to_digit(card[0], manual)
 
-    def calculate_point(self) -> None:
-        self.point += self._convert_to_digit(self.hand[-1][0])
+    def calculate_point(self, manual: bool = True) -> None:
+        self.point += self._convert_to_digit(self.hand[-1][0], manual)
 
-    def _convert_to_digit(self, value: str) -> int:
+    def _convert_to_digit(self, value: str, manual: bool) -> int:
         if value.isdigit():
             return int(value)
         elif value == "A":
-            return self._convert_ace()
+            return self._convert_ace(manual)
         return 10
 
-    def _convert_ace(self) -> int:
-        answer: str = input("Voce deseja que o 'Ás' valha 1 ou 11? ")
-        if answer == "1":
+    def _convert_ace(self, manual: bool) -> int:
+        if manual:
+            answer: str = input("Voce deseja que o 'Ás' valha 1 ou 11? ")
+            if answer == "1":
+                return 1
+            elif answer == "11":
+                return 11
+            print("Valor invalido, deve ser 1 ou 11")
+            return self._convert_ace(manual)
+        else:
+            if self.point + 11 <= 21:
+                return 11
             return 1
-        elif answer == "11":
-            return 11
-        print("Valor invalido, deve ser 1 ou 11")
-        return self._convert_ace()
+
+    def compare_results(self, opponent, coin: int) -> int:
+        if self.point > opponent.point:
+            print("\nVoce venceu, parabens!")
+            coin += 1
+        elif self.point < opponent.point:
+            print("\nVoce perdeu")
+            coin -= 1
+        else:
+            print("\nEmpate")
+        print("Sua mao ->")
+        self.show_hand()
+        print(f"Total de pontos: {self.point}")
+        print("Mao do oponente ->")
+        opponent.show_hand()
+        print(f"Total de pontos: {opponent.point}")
+        return coin
